5aa229d5b08f659c0f981b4d76009a921f0a38c1
Aditya Chatterjee <adityachatterjee891@gmail.com>
2024-07-31 12:08:58 +0530

    Ep2

---
+++ Episode 02 - Igniting Our App/App.js
@@ -0,0 +1,29 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const heading = React.createElement(
+    "h1",
+    {id:"heading"},
+    "Hello World from React!"
+);
+
+const parent = React.createElement(
+    "div",
+    {id : "parent"},
+    [React.createElement(
+        "div",
+        {id:"child"},
+        [
+            React.createElement("h1",{},"I am an H1 tag"),
+            React.createElement("h2",{},"I am an H2 tag")
+        ]
+    )]
+);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(
+    document.getElementById("root")
+);
+
+root.render(parent);
