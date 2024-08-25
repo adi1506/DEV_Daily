7b80428b797cd435513d84c2fd420dff0b14b442
Aditya Chatterjee <adityachatterjee891@gmail.com>
2024-08-25 11:14:37 +0530

    ep6-update

--- Episode 06 - Exploring The World/src/Components/Body.js
+++ Episode 06 - Exploring The World/src/Components/Body.js
@@ -65,7 +65,7 @@ const Body = () => {
 						const filteredList = listOfResState.filter(
 							(res) => res.info.avgRating > 4.2
 						);
-						setListOfResState(filteredList);
+						setFilteredRes(filteredList);
 						console.log(listOfResState);
 					}}
 				>
